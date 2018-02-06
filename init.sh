ae39b3405d7b912816b3bb74b8cd3bb49177f2ba
Gymnae <dagorwaith@gmail.com>
2018-02-06 13:16:20 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 
-# Inspired by https://github.com/psi-4ward/docker-owncloud
-# more than ripped off, i admit
+#optimize www.conf of php
+sed -i "s/pm.max_children = .*/pm.max_children = 240/" /etc/php7/php-fpm.d/www.conf
+sed -i "s/pm.start_servers = .*/pm.start_servers = 20/" /etc/php7/php-fpm.d/www.conf
+sed -i "s/pm.min_spare_servers = .*/pm.min_spare_servers = 10/" /etc/php7/php-fpm.d/www.conf
+sed -i "s/pm.max_spare_servers = .*/pm.max_spare_servers = 20/" /etc/php7/php-fpm.d/www.conf
+sed -i "s/;pm.max_requests = 500/pm.max_requests = 500/" /etc/php7/php-fpm.d/www.conf
 
-# enable opcache config
-sed -i -e"s/;opcache.enable=.*/opcache.enable=1/" /etc/php7/php.ini
-sed -i -e"s/;opcache.enable_cli=.*/opcache.enable_cli=1/" /etc/php7/php.ini
-sed -i -e"s/;opcache.interned_strings_buffer=.*/opcache.interned_strings_buffer=8/" /etc/php7/php.ini
-sed -i -e"s/;opcache.max_accelerated_files=.*/opcache.max_accelerated_files=10000/" /etc/php7/php.ini
-sed -i -e"s/;opcache.memory_consumption=.*/opcache.memory_consumption=128/" /etc/php7/php.ini
-sed -i -e"s/;opcache.save_comments=.*/opcache.save_comments=1/" /etc/php7/php.ini
-sed -i -e"s/;opcache.revalidate_freq=.*/opcache.revalidate_freq=1/" /etc/php7/php.ini
+# optimize php.ini
+sed -i "s/output_buffering =.*/output_buffering = Off/" /etc/php7/php.ini
+sed -i "s/max_execution_time =.*/max_execution_time = 1800/" /etc/php7/php.ini
+sed -i "s/max_input_time =.*/max_input_time = 3600/" /etc/php7/php.ini
+sed -i "s/post_max_size =.*/post_max_size = 4G/" /etc/php7/php.ini
+sed -i "s/;upload_tmp_dir =.*/upload_tmp_dir = \/upload_tmp/" /etc/php7/php.ini
+sed -i "s/upload_max_filesize =.*/upload_max_filesize = 4G/" /etc/php7/php.ini
+sed -i "s/max_file_uploads =.*/max_file_uploads = 100/" /etc/php7/php.ini
+sed -i "s/;date.timezone.*/date.timezone = Europe\/\Berlin/" /etc/php7/php.ini
+sed -i "s/;session.cookie_secure.*/session.cookie_secure = True/" /etc/php7/php.ini
+sed -i "s/;opcache.enable=.*/opcache.enable=1/" /etc/php7/php.ini
+sed -i "s/;opcache.enable_cli=.*/opcache.enable_cli=1/" /etc/php7/php.ini
+sed -i "s/;opcache.memory_consumption=.*/opcache.memory_consumption=128/" /etc/php7/php.ini
+sed -i "s/;opcache.interned_strings_buffer=.*/opcache.interned_strings_buffer=8/" /etc/php7/php.ini
+sed -i "s/;opcache.max_accelerated_files=.*/opcache.max_accelerated_files=10000/" /etc/php7/php.ini
+sed -i "s/;opcache.revalidate_freq=.*/opcache.revalidate_freq=1/" /etc/php7/php.ini
+sed -i "s/;opcache.save_comments=.*/opcache.save_comments=1/" /etc/php7/php.ini
 
 # needed for some apps, which don't set x-frame themselves. potential security risk
 #sed -i -e"s/header('X-Frame-Options: .*/header('X-Frame-Options: ALLOW');/" /usr/share/webapps/nextcloud/lib/private/legacy/response.php
-
 
 # deal with the weird alpine packaging of some nextcloud packages
 ln -s /usr/share/doc/nextcloud/core/doc/ /usr/share/webapps/nextcloud/core/doc
@@ -50,10 +62,6 @@ ln -s /media/owncloud/apps /usr/share/webapps/nextcloud/apps2
       && mkdir -p /media/owncloud/apps \
       && chown -R nginx:www-data /media/owncloud/apps \
       && chmod -R 0770 /media/owncloud/apps
-      
-# fix owncloud bugs
-#mv /usr/share/webapps/owncloud/apps/calendar-* /usr/share/webapps/owncloud/apps/calendar
-#mv /usr/share/webapps/owncloud/apps/contacts-* /usr/share/webapps/owncloud/apps/contacts
 
 
 # owncloud log directory
@@ -63,8 +71,7 @@ ln -s /media/owncloud/apps /usr/share/webapps/nextcloud/apps2
       && chmod 0770 /media/owncloud/logs/owncloud
       
 # create cronjob for nextcloud and run crond
-{ crontab -l -u nginx; echo '*/15  *  *  *  * php -f /usr/share/webapps/nextcloud/cron.php'; } | crontab -u nginx -
-crond -b -l 0 -L /var/log/cron.log
+{ crontab -l -u nginx; echo '*/15  *  *  *  * php -f /usr/share/webapps/nextcloud/cron.php'; } | crontab -u nginx -crond -b -l 0 -L /var/log/cron.log
 
 # only needed, if you don't have redis as a server  start redis and make it run as deamon
 #mkdir -p /media/owncloud/cache
