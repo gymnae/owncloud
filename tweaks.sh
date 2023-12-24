ff54e0d796ce34b922da3e7b6ad726b408e9cbe6
Gunnar <dagorwaith@gmail.com>
2023-12-24 23:37:08 +0100

    Update tweaks.sh

--- tweaks.sh
+++ tweaks.sh
@@ -85,6 +85,12 @@ session.save_path = "unix:///run/redis-socket/redis.sock?persistent=1&weight=1&d
 # sed -i '\|this->encryptionManager->isEnabled|,/}/ s/^#*/#/' /var/www/html/apps/previewgenerator/lib/Command/Generate.php#
 # sed -i '\|this->encryptionManager->isEnabled|,/}/ s/^#*/#/' /var/www/html/apps/memories/lib/Command/Index.php
 
+# give access to the intel iGPU transcoding device to www-data
+GID=`stat -c "%g" /dev/dri/renderD128`
+groupadd -g $GID render2 || true # sometimes this is needed
+GROUP=`getent group $GID | cut -d: -f1`
+usermod -aG $GROUP www-data
+
 # Start a subshell or background shell
 (
   # Sleep for 24 hours
