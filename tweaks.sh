71bbb701fc2ba993505dab87aa333c84b3a4462f
Gunnar <dagorwaith@gmail.com>
2024-01-09 10:32:09 +0100

    Update tweaks.sh

--- tweaks.sh
+++ tweaks.sh
@@ -86,7 +86,7 @@ session.save_path = "unix:///run/redis-socket/redis.sock?persistent=1&weight=1&d
 # sed -i '\|this->encryptionManager->isEnabled|,/}/ s/^#*/#/' /var/www/html/apps/memories/lib/Command/Index.php
 
 # give access to the intel iGPU transcoding device to www-data
-addgroup --gid 486--quiet --system render
+addgroup --gid 486 --quiet --system render
 GID=`stat -c "%g" /dev/dri/renderD128`
 # groupadd -g $GID render2 || true
 GROUP=`getent group $GID | cut -d: -f1`
